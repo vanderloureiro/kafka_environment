10987b50f68998634ad13252b4bf6d19860efa9b
Vanderlei Loureiro <vanderloureiro.dev@gmail.com>
2021-04-12 14:07:01 -0300

    Create RegisterController

--- register-message-service/src/main/java/com/br/registermessageservice/gateway/http/RegisterController.java
+++ register-message-service/src/main/java/com/br/registermessageservice/gateway/http/RegisterController.java
@@ -1,5 +1,11 @@
 package com.br.registermessageservice.gateway.http;
 
+import com.br.registermessageservice.gateway.dto.MessageDto;
+import com.br.registermessageservice.gateway.form.MessageForm;
+import com.br.registermessageservice.service.RegisterMessageService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/register")
 public class RegisterController {
     
+    private RegisterMessageService registerMessageService;
+
+    public RegisterController(RegisterMessageService registerMessageService) {
+        this.registerMessageService = registerMessageService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MessageDto> sendMessage(MessageForm form) {
+        return ResponseEntity.ok().body(this.registerMessageService.execute(form));
+    }
 }
--- register-message-service/src/main/java/com/br/registermessageservice/service/RegisterMessageService.java
+++ register-message-service/src/main/java/com/br/registermessageservice/service/RegisterMessageService.java
@@ -1,11 +1,15 @@
 package com.br.registermessageservice.service;
 
+import com.br.registermessageservice.gateway.dto.MessageDto;
+import com.br.registermessageservice.gateway.form.MessageForm;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegisterMessageService {
     
-    public void execute() {
+    public MessageDto execute(MessageForm form) {
         // todo
+        return null;
     }
 }
